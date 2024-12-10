16433e6ac6c88444ce7a0a1fab4ee2fe1cdb762c
Fausto Carcassi <fausto.carcassi@gmail.com>
2024-12-10 17:21:37 +0100

    Update snelliusjob.sh

--- server/snellius/snelliusjob.sh
+++ server/snellius/snelliusjob.sh
@@ -3,6 +3,8 @@
 #SBATCH -p rome
 #SBATCH -t 100:00:00
 
+# segfault happens when the stack size is too small
+# so I set it to unlimited
 ulimit -s unlimited
 
 # Run this code in a file with source setup.sh
@@ -16,11 +18,20 @@ module load parallel/20220722-GCCcore-11.3.0
 
 # Compile script (assuming sbatch is run from the ./server/snellius directory)
 cd ../../
-make
+make debug
 
-# ID is a string with current time
-ID=$(date +"%Y%m%d_%H%M%S")
-./main \
+# # ID is a string with current time
+# ID=$(date +"%Y%m%d_%H%M%S")
+# ./main \
+# 	--steps 			100000 	 		\
+# 	--nobs 				50	 	 		\
+# 	--csize 			5 		 		\
+# 	--likelihoodweight 	30	 			\
+# 	--searchdepth		2		 		\
+# 	--fname 			"data/${ID}"	\
+# 	--ct				16
+
+gdb -batch -ex "run" -ex "thread apply all backtrace full" -ex "quit" ./main \
 	--steps 			100000 	 		\
 	--nobs 				50	 	 		\
 	--csize 			5 		 		\
@@ -29,17 +40,4 @@ ID=$(date +"%Y%m%d_%H%M%S")
 	--fname 			"data/${ID}"	\
 	--ct				16
 	
-RET=$?
-if [ $RET -ne 0 ]; then
-    if [ -f core ]; then
-        echo "Program crashed with exit code $RET. Generating backtrace..."
-        gdb -batch -ex "thread apply all backtrace full" -ex "quit" ./main core > backtrace.txt
-        echo "Backtrace saved to backtrace.txt"
-    else
-        echo "Program crashed but no core file found."
-    fi
-fi
-
 # ./main --steps 10000 --nobs 5 --csize 5 --likelihoodweight 30 --searchdepth 2 --fname "data/10" --ct 16
-
-# ./main --steps 10000 --nobs 100 --likelihoodweight 50 --chains 16
