f1bd20bfa44b82105259934c91b43942eb51b5b2
Fausto Carcassi <fausto.carcassi@gmail.com>
2025-08-17 19:33:45 +0200

    Update runjobs.sh

--- server/snellius/runjobs.sh
+++ server/snellius/runjobs.sh
@@ -10,7 +10,8 @@ for k in 0 1; do
     # run jobs for 20, 30, 40 likelihood weight
     for j in 20 30 40; do
         # run jobs for 2, 3, 4, 5 quantifiers
-        for i in 2 3 4 5; do
+        # for i in 2 3 4 5; do
+		for i in 5; do
             sbatch snelliusjob.sh $j $i $k
             # wait 1 second
             sleep 1
